5737c4dd60a160bc9c61be18b1468571b801d8c9
Aditi-panta <88805173+Aditi-panta@users.noreply.github.com>
2022-06-11 12:53:45 -0400

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,40 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyAmTq0obNbkoHJzYW2YbzbDxj8NCgjDnxM",
+      authDomain: "kwitter-1afda.firebaseapp.com",
+      databaseURL: "https://kwitter-1afda-default-rtdb.firebaseio.com",
+      projectId: "kwitter-1afda",
+      storageBucket: "kwitter-1afda.appspot.com",
+      messagingSenderId: "440030630088",
+      appId: "1:440030630088:web:a5178cfb80321244ef15fd"
+    };
+    
+    // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+   user_name=localStorage.getItem("user_name");
+   room_name=localStorage.getItem("room_name");
+
+   function send(){
+         msg=document.getElementById("msg").value;
+         firebase.database().ref(room_name).push({
+               name:user_name,
+               message:msg,
+               like:0
+         });
+         document.getElementById("msg").value="";
+   }
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
